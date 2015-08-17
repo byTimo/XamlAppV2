f06a71ce0258383ce38e5189b0c58daf9f871a59
byTimo <race.timo@gmail.com>
2015-08-18 00:50:59 +0500

    *вкладки как надо +кнопочка настроек

---
+++ ZappChat_v3/Controls/ValueConverters/TabSizeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ZappChat_v3.Controls.ValueConverters
+{
+    public class TabSizeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            TabControl tabControl = values[0] as TabControl;
+            double width = tabControl.ActualWidth / tabControl.Items.Count;
+            //Subtract 1, otherwise we could overflow to two rows.
+            return (width <= 1) ? 0 : (width - 2);
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
